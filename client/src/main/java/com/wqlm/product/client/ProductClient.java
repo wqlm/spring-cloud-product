e65fd15a5d6df04f132c02847128ce14a789b738
wqlm <649924325@qq.com>
2019-08-13 18:52:02 +0800

    6-4 Spring Cloud Bus  刷新配置

--- client/src/main/java/com/wqlm/product/client/ProductClient.java
+++ client/src/main/java/com/wqlm/product/client/ProductClient.java
@@ -2,7 +2,8 @@ package com.wqlm.product.client;
 
 import com.wqlm.product.common.DecreaseStockInput;
 import com.wqlm.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
