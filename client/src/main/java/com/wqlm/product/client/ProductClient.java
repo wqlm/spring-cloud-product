82bdd6853caee66b9df6cf56752c43f97043cf4f
wqlm <649924325@qq.com>
2019-08-19 18:44:15 +0800

    10-6 feign-hystrix的使用

--- client/src/main/java/com/wqlm/product/client/ProductClient.java
+++ client/src/main/java/com/wqlm/product/client/ProductClient.java
@@ -3,13 +3,14 @@ package com.wqlm.product.client;
 import com.wqlm.product.common.DecreaseStockInput;
 import com.wqlm.product.common.ProductInfoOutput;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -17,4 +18,19 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+
+    @Component // 记得打上该组件
+    class ProductClientFallback implements ProductClient{
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
